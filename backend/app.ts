f18a5305c4061c17910e065d7b4c5902b9198fe7
yiwywo18ypwtp1 <negalapetrovna77@gmail.com>
2026-05-06 16:17:15 +0300

    fix(deploy): fix origin url in cors

--- backend/app.ts
+++ backend/app.ts
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5001;
 app.use(cors({
     origin: [
         "http://localhost:3000",
-        "books-trade-platform-o2eo.vercel.app"
+        "https://books-trade-platform-o2eo.vercel.app"
     ],
     credentials: true,
 }));
