069688b0b3cd643f3fd5011c638b2dbb81188d82
yiwywo18ypwtp1 <negalapetrovna77@gmail.com>
2026-05-06 17:47:46 +0300

    fix(cors): add missed PATCH method in allowed headers

--- backend/app.ts
+++ backend/app.ts
@@ -19,7 +19,7 @@ app.use(cors({
         "https://books-trade-platform-o2eo.vercel.app"
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
